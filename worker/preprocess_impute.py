d21b7e2e9cbdbe583d08355949d20d046d73e596
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2022-11-28 16:58:22 -0700

    Create preprocess_impute.py

---
+++ worker/preprocess_impute.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+
+class Preprocessor:
+    """
+        This class shall  be used to clean and transform the data before training.
+
+        Written By: iNeuron Intelligence
+        Version: 1.0
+        Revisions: None
+
+        """
+
+    def __init__(self, file_object, logger_object):
+        self.file_object = file_object
+        self.logger_object = logger_object
+
+    def impute_missing_values(self, data):
+        """
+                                        Method Name: impute_missing_values
+                                        Description: This method replaces all the missing values in the Dataframe using KNN Imputer.
+                                        Output: A Dataframe which has all the missing values imputed.
+                                        On Failure: Raise Exception
+
+                                        Written By: iNeuron Intelligence
+                                        Version: 1.0
+                                        Revisions: None
+                     """
+        self.logger_object.log(self.file_object, 'Entered the impute_missing_values method of the Preprocessor class')
+        self.data = data
+        try:
+            imputer = KNNImputer(n_neighbors = 3, weights = 'uniform', missing_values = np.nan)
+            self.new_array = imputer.fit_transform(self.data) # impute the missing values
+            # convert the nd-array returned in the step above to a Dataframe
+            self.new_data = pd.DataFrame(data = self.new_array, columns = self.data.columns)
+            self.logger_object.log(self.file_object, 'Imputing missing values Successful. Exited the impute_missing_values method of the Preprocessor class')
+            return self.new_data
+        except Exception as e:
+            self.logger_object.log(self.file_object,'Exception occured in impute_missing_values method of the Preprocessor class. Exception message:  ' + str(e))
+            self.logger_object.log(self.file_object,'Imputing missing values failed. Exited the impute_missing_values method of the Preprocessor class')
+            raise Exception()
+
+            

bd6b3f2ef1cea454bdd37fefe0f28cf340be2f05
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2022-11-30 19:18:22 -0700

    Working training code

--- worker/train/training_model.py
+++ worker/train/training_model.py
@@ -1,25 +1,45 @@
 from sklearn.model_selection import train_test_split
+# from file_operations import file_methods
 import tuner
 import pandas as pd
 import pickle
-
+from google.cloud import storage
+from google.oauth2 import service_account
+import os
+import sys
 
 class trainModel:
     def __init__(self):
         pass
 
-    def trainingModel(self):
+    def trainingModel(self, file):
         try:
             # Splitting the data into training and test set for each cluster one by one
-            df = pd.read_csv('cleaned.csv')
-            x_train, x_test, y_train, y_test = train_test_split(df.drop('Output', axis = 1), df.Output, test_size=1 / 3, random_state=36)
+            credentials = service_account.Credentials.from_service_account_file(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+            scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
+            storage_client = storage.Client()
+            bucket_name = 'dtsc-auto-ml'
+            bucket = storage_client.get_bucket(bucket_name)
 
-            # model_finder = tuner.Model_finder.get_best_model(x_train, x_test, y_train, y_test)    #### Object intialization
+            blobs = list(bucket.list_blobs(prefix = 'preprocess/', delimiter='/'))
+            blob = bucket.blob(f'preprocess/cleaned_{file}.csv')
+            blob.download_to_filename(f'cleaned_{file}.csv')
 
-            #### getting the best model 
+            df = pd.read_csv(f'cleaned_{file}.csv')
+            x_train, x_test, y_train, y_test = train_test_split(df.drop('Output', axis = 1)._get_numeric_data(), df.Output, test_size=1 / 3, random_state=36)
+
             best_model_name, best_model = tuner.Model_finder().get_best_model(x_train, y_train, x_test, y_test)
-            pickle.dump(best_model, open(f'{best_model}_model', 'wb'))
+            
+            pickle.dump(best_model, open(f'{file}_model', 'wb'))
+            os.remove(f'cleaned_{file}.csv')
+            blob = bucket.blob(f'{file}_model')
+            blob.upload_from_filename(f'{file}_model')
+            os.remove(f'{file}_model')
+            # file_op = file_methods.File_Operation(self.file_object, self.log_writer)
+            # save_model = file_op.save_model(best_model, best_model_name)
         except Exception:
-            print(f'This is the error that we have received from the upper management {Exception}\nPlease try again later!!!')
+            print('Error training the model')
+            
+a = trainModel().trainingModel(sys.argv[1])
+
         
-a = trainModel().trainingModel()

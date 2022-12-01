c1b73e1647d775e084c35e0267e8dd3c3aa3f413
Thejas B K <50453786+ThejasBK@users.noreply.github.com>
2022-11-30 19:17:07 -0700

    Working prediction code

--- worker/test/test.py
+++ worker/test/test.py
@@ -1,12 +1,35 @@
 import pickle
 from sklearn.metrics import r2_score, accuracy_score
 import pandas as pd
+from google.cloud import storage
+from google.oauth2 import service_account
+import os
+import sys
 
 class testModel:
-    def predictions(self, best_model, data):
-        saved_model = pickle.load(open(f'{best_model}_model', 'rb'))
-        preds = saved_model.predict(data)
-        print(preds)
+    def predictions(self, file):
+        try:
+            credentials = service_account.Credentials.from_service_account_file(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+            scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
+            storage_client = storage.Client()
+            bucket_name = 'dtsc-auto-ml'
+            bucket = storage_client.get_bucket(bucket_name)
+
+            blobs = list(bucket.list_blobs(prefix = 'models/', delimiter='/'))
+            blob = bucket.blob(f'{file}_model')
+            blob.download_to_filename(f'{file}_model')
+
+            blobs = list(bucket.list_blobs(prefix = 'preprocess/', delimiter='/'))
+            blob = bucket.blob(f'preprocess/cleaned_{file}.csv')
+            blob.download_to_filename(f'cleaned_{file}.csv')
+
+            df = pd.read_csv(f'cleaned_{file}.csv')
+            saved_model = pickle.load(open(f'{file}_model', 'rb'))
+            preds = saved_model.predict(df.drop('Output', axis = 1)._get_numeric_data())
+            print(preds)
+            os.remove(f'cleaned_{file}.csv')
+            os.remove(f'{file}_model')
+        except:
+            print('Error generating predictions')
         
-data = pd.read_csv('cleaned_test_dataset.csv').drop('Output', axis = 1)
-testModel().predictions('RandomForestClassifier(max_depth=8, max_features=None, n_estimators=64)', data)
+testModel().predictions(sys.argv[1])
